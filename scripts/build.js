78d1de95a24e65bbceb17c61f62ae6b11ec27965
Junior <jnrplay@hotmail.com>
2025-02-18 22:42:02 -0300

    chore: esm

--- scripts/build.js
+++ scripts/build.js
@@ -1,20 +1,18 @@
-import { build } from 'esbuild';
-import pkg from 'npm-dts';
-
-
+import { build } from 'esbuild'
+import pkg from 'npm-dts'
 
 new pkg.Generator({
   entry: 'src/index.ts',
-  output: 'dist/index.d.ts',
-}).generate();
+  output: 'dist/index.d.ts'
+}).generate()
 
 build({
-  entryPoints: ["src/index.ts"],
-  outfile: './dist/index.js',
+  entryPoints: ['src/index.ts'],
+  outfile: 'dist/index.esm.js',
   format: 'cjs',
   platform: 'browser',
   minify: true,
   bundle: true,
   treeShaking: true,
-  tsconfig: './tsconfig.json',
+  tsconfig: './tsconfig.json'
 })
